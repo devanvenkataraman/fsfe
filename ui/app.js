893ea1df569e66574ecec04edd631338b7c588d0
devan <devan.venkataraman@tufts.edu>
2020-06-01 22:54:46 +0000

    sanity check

--- ui/app.js
+++ ui/app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-	res.send('did THIS in vsc!');
+	res.send('did THIS in vsc???');
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
